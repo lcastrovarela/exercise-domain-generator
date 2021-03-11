84b6dbfce628ee86119b46a63e33bee2c8cec3ee
lcastrovarela <lcastrovarela@gmail.com>
2021-03-11 14:14:35 +0000

    commit with code first ver

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//write your code here
+
+let pronoun = ["the", "our", "my"];
+let adj = ["great", "big", "huge"];
+let noun = ["jogger", "racoon", "swimmer"];
+let domain = ["com", "net", "us"];
+
+const theDomainNameGenerator = () => {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let e = 0; e < adj.length; e++) {
+      for (let x = 0; x < noun.length; x++) {
+        for (let y = 0; y < domain.length; y++) {
+          console.log(pronoun[i] + adj[e] + noun[x] + "." + domain[y]);
+        }
+      }
+    }
+  }
 };
+console.log(theDomainNameGenerator());
